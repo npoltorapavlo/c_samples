a6c1ff8e8578380433ccb64eb4f59c2facfe7860
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2021-07-06 01:54:23 +0300

    update sockets sample

--- sockets/sockets.cpp
+++ sockets/sockets.cpp
@@ -8,26 +8,24 @@
 using namespace std;
 
 namespace {
-  const int socketPort = 8099;
-  const char* socketFile = "/tmp/test123";
   const int backlog = 5;
   const int bufSize = 10;
-  const char* testData = "test";
+  const char *testData = "test";
 
-  template <class SOCKET, class ADDR_ARG>
+  template<class SOCKET, class ADDR_ARG>
   void test(ADDR_ARG arg1, int numClients, int testTimeMs) {
     list<thread> threads;
     list<SOCKET> sockets(numClients + 1);
 
     // 1. Create N sockets
-    for_each(sockets.begin(), sockets.end(), []( SOCKET& it) { it.Create(); });
+    for_each(sockets.begin(), sockets.end(), [](SOCKET &it) { it.Create(); });
 
     // 2. Socket1 listens, thread accepts connections
     sockets.front().Listen(arg1, backlog);
     threads.emplace_back([&]() { sockets.front().Accept(); });
 
     // 3. Socket2..N connect and write a message, thread receives data
-    for_each(std::next(sockets.begin()), sockets.end(), [&](SOCKET& it) {
+    for_each(std::next(sockets.begin()), sockets.end(), [&](SOCKET &it) {
       it.Connect(arg1);
       threads.emplace_back([&]() { it.Receive(bufSize); });
       it.Write(testData, strlen(testData));
@@ -47,10 +45,16 @@ namespace {
 }
 
 int main(int argc, char** argv) {
-  int numClients = ((argc > 1) ? atoi(argv[1]) : 1);
-  int testTimeMs = ((argc > 2) ? atoi(argv[2]) : 2000);
+  const long int port = strtol(argv[1], nullptr, 10);
+  const char* file = ((port == 0) ? argv[1] : nullptr);
+  const int numClients = ((argc > 2) ? atoi(argv[2]) : 1);
+  const int testTimeMs = ((argc > 3) ? atoi(argv[3]) : 2000);
 
-//  test<EchoSocket<WebSocket>>(socketPort, numClients, testTimeMs);
-  test<EchoSocket<UnixSocket>>(socketFile, numClients, testTimeMs);
+  if (port == 0) {
+    test<EchoSocket<UnixSocket>>(file, numClients, testTimeMs);
+  } else {
+    test<EchoSocket<WebSocket>>(port, numClients, testTimeMs);
+  }
+
   return 0;
 }
