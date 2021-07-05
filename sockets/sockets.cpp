ce8ecf68d784852ea7bc676745ef6e4cddd058b3
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2021-07-06 02:05:47 +0300

    update comments

--- sockets/sockets.cpp
+++ sockets/sockets.cpp
@@ -17,21 +17,21 @@ namespace {
     list<thread> threads;
     list<SOCKET> sockets(numClients + 1);
 
-    // 1. Create N sockets
+    // 1. Create N+1 sockets
     for_each(sockets.begin(), sockets.end(), [](SOCKET &it) { it.Create(); });
 
-    // 2. Socket1 listens, thread accepts connections
+    // 2. Socket0 listens, thread accepts connections
     sockets.front().Listen(arg1, backlog);
     threads.emplace_back([&]() { sockets.front().Accept(); });
 
-    // 3. Socket2..N connect and write a message, thread receives data
+    // 3. Socket1..N connect and write a message, thread receives data
     for_each(std::next(sockets.begin()), sockets.end(), [&](SOCKET &it) {
       it.Connect(arg1);
       threads.emplace_back([&]() { it.Receive(bufSize); });
       it.Write(testData, strlen(testData));
     });
 
-    // 4. Delete first socket
+    // 4. Delete Socket0
     threads.emplace_back([&]() {
       std::this_thread::sleep_for(std::chrono::milliseconds(testTimeMs));
       sockets.pop_front();
